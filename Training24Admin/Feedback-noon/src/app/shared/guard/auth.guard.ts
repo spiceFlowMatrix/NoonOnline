0d04f7edd9db34cf30301610190c9fb546ef859a
saksham.sdd <sakshams@smartdatainc.net>
2020-03-27 21:35:17 +0530

    Changed default application route to /web/sales, /web/manage, /web/feedback

--- Training24Admin/Feedback-noon/src/app/shared/guard/auth.guard.ts
+++ Training24Admin/Feedback-noon/src/app/shared/guard/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
             return true;
         }
 
-        this.router.navigate(['/feed']);
+        this.router.navigate(['/login']);
         return false;
     }
 }
