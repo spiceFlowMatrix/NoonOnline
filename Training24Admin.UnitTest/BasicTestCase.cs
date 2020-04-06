952efcf8a3f4ec86c7a564f6471e509b6970338f
Nirav Dudhat <nirav@iblinfotech.email>
2020-04-06 22:15:54 +0530

    -> Added unit test project in Noon main solution -> Added unit test case method for validate null value in chapter model -> Create method for check null string value from model in unit test case

---
+++ Training24Admin.UnitTest/BasicTestCase.cs
@@ -0,0 +1,46 @@
+using System;
+using Trainning24.Domain.Entity;
+using Xunit;
+
+namespace Training24Admin.UnitTest
+{
+    public class BasicTestCase
+    {
+        /// <summary>
+        /// Unit test for check chapter require value
+        /// Parms Chapter chapter = new Chapter()
+        /// This will define chapter as null object and it's check null value of model data
+        /// </summary>
+        [Fact]
+        public void Chapter_Require_Field()
+        {
+            //Chapter chapter = new Chapter{
+            //    Name = "Test",
+            //    Code = "A1592020"
+            //};
+            Chapter chapter = new Chapter();
+            Assert.Throws<ArgumentException>(() => CheckIfNullOrEmpty(chapter));
+        }
+
+        /// <summary>
+        /// Method for checking null value of chapter model 
+        /// Quiz Id, ItemOrder values are optional here
+        /// </summary>
+        /// <param name="chapter"></param>
+        public void CheckIfNullOrEmpty(Chapter chapter)
+        {
+            if (string.IsNullOrEmpty(chapter.Name))
+            {
+                throw new ArgumentException();
+            }
+            if (string.IsNullOrEmpty(chapter.Code))
+            {
+                throw new ArgumentException();
+            }
+            if (chapter.CourseId == 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
