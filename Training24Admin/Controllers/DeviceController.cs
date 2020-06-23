9b4f1a041c180193d24ab873554e22106d9e645d
Dipak Italiya <dipak@iblinfotech.email>
2020-06-23 18:50:41 +0530

    Minor bug fix

--- Training24Admin/Controllers/DeviceController.cs
+++ Training24Admin/Controllers/DeviceController.cs
@@ -24,7 +24,6 @@ namespace Training24Admin.Controllers
 
         private readonly LessonBusiness LessonBusiness;
         private readonly DeviceBusiness deviceBusiness;
-        public static List<string> UniqMacList = new List<string>();
 
 
         public DeviceController(LessonBusiness LessonBusiness, DeviceBusiness deviceBusiness
@@ -116,35 +115,13 @@ namespace Training24Admin.Controllers
                 {
                     if (!String.IsNullOrEmpty(objData.macAddress) && !String.IsNullOrEmpty(objData.ipAddress))
                     {
-                        if (!UniqMacList.Contains(objData.macAddress))
+                        var deviceExist = deviceBusiness.GetDevicesByMacAdd(objData.macAddress, int.Parse(tc.Id));
+                        if (deviceExist == null)
                         {
-                            UniqMacList.Add(objData.macAddress);
-                            var deviceExist = deviceBusiness.GetDevicesByMacAdd(objData.macAddress, int.Parse(tc.Id));
-                            if (deviceExist == null)
+                            if (deviceBusiness.CheckDeviceQuota(int.Parse(tc.Id)) > 0)
                             {
-                                if (deviceBusiness.CheckDeviceQuota(int.Parse(tc.Id)) > 0)
-                                {
-                                    UniqMacList.Remove(objData.macAddress);
-                                    var deviceDetails = deviceBusiness.Create(objData, int.Parse(tc.Id));
-                                    successResponse.data = deviceDetails;
-                                    successResponse.response_code = 0;
-                                    successResponse.message = "Device registered and activated";
-                                    successResponse.status = "Success";
-                                    return StatusCode(200, successResponse);
-                                }
-                                else
-                                {
-                                    UniqMacList.Remove(objData.macAddress);
-                                    unsuccessResponse.response_code = 3;
-                                    unsuccessResponse.message = "you are out of device quota";
-                                    unsuccessResponse.status = "Unsuccess";
-                                    return StatusCode(406, unsuccessResponse);
-                                }
-                            }
-                            else if (deviceExist != null && deviceExist.IsDeleted != true)
-                            {
-                                UniqMacList.Remove(objData.macAddress);
-                                successResponse.data = deviceExist;
+                                var deviceDetails = deviceBusiness.Create(objData, int.Parse(tc.Id));
+                                successResponse.data = deviceDetails;
                                 successResponse.response_code = 0;
                                 successResponse.message = "Device registered and activated";
                                 successResponse.status = "Success";
@@ -152,39 +129,43 @@ namespace Training24Admin.Controllers
                             }
                             else
                             {
-                                UniqMacList.Remove(objData.macAddress);
-                                unsuccessResponse.response_code = 2;
-                                unsuccessResponse.message = "This device has been deactivated.";
+                                unsuccessResponse.response_code = 3;
+                                unsuccessResponse.message = "you are out of device quota";
                                 unsuccessResponse.status = "Unsuccess";
                                 return StatusCode(406, unsuccessResponse);
                             }
                         }
+                        else if (deviceExist != null && deviceExist.IsDeleted != true)
+                        {
+                            successResponse.data = deviceExist;
+                            successResponse.response_code = 0;
+                            successResponse.message = "Device registered and activated";
+                            successResponse.status = "Success";
+                            return StatusCode(200, successResponse);
+                        }
                         else
                         {
-                            unsuccessResponse.response_code = 1;
-                            unsuccessResponse.message = "This user's request already in the queue.";
+                            unsuccessResponse.response_code = 2;
+                            unsuccessResponse.message = "This device has been deactivated.";
                             unsuccessResponse.status = "Unsuccess";
                             return StatusCode(406, unsuccessResponse);
                         }
                     }
                     else
                     {
-                        UniqMacList.Remove(objData.macAddress);
                         unsuccessResponse.response_code = 4;
                         unsuccessResponse.message = "Invalid input";
                         unsuccessResponse.status = "Failure";
-                        return StatusCode(500, unsuccessResponse);
+                        return StatusCode(406, unsuccessResponse);
                     }
                 }
                 else
                 {
-                    UniqMacList.Remove(objData.macAddress);
                     return StatusCode(406, ModelState);
                 }
             }
             catch (Exception ex)
             {
-                UniqMacList.Remove(objData.macAddress);
                 unsuccessResponse.response_code = 2;
                 unsuccessResponse.message = ex.Message;
                 unsuccessResponse.status = "Failure";
